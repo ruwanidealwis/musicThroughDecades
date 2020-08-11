bb8f368f976541c2c2e784b7baf1874cdbf31276
Ruwani <ruwanide51@gmail.com>
2020-08-11 13:13:20 -0400

    added spotifyController basics

--- server/controllers/spotifyController.js
+++ server/controllers/spotifyController.js
@@ -1,10 +1,31 @@
 let SpotifyWebApi = require("spotify-web-api-node");
+var spotifyApi = new SpotifyWebApi();
 let clientId = process.env.CLIENTID;
 let clientSecret = process.env.CLIENTSECRET;
-let redirectUri = "https://colouryourplaylists.herokuapp.com/callback";
+let redirectUri = "http://localhost:8000/callback"; //change this when running on heroku
+let scopes = ["playlist-read-private", "user-read-email"]; //get the users account and their private playlists
 //running locally
 if (process.env.PORT == null) {
   clientId = config.clientID;
   clientSecret = config.clientSecret;
   redirectUri = "http://localhost:8000/callback";
 }
+
+analyzeComparators = (comparators) => {
+  let userSpotify = ["all-time", "6 months", "1 month"];
+  if (
+    userSpotify.includes(comparators[0]) ||
+    userSpotify.includes(comparators[1]) //determines if there needs to be a specical scope
+  ) {
+    //need to redirect to login and get a callback
+    //need specical scope authorization... (user top read)
+    //after also get top music info
+  }
+  //at least one paramter has to be a year so following method is always called
+  if (!userSpotify.includes(comparators[0])) {
+    //this must mean it is a year.... (check if database has it, otherwise call python webscraper)
+  }
+
+  if (!userSpotify.includes(comparators[0])) {
+  }
+};
