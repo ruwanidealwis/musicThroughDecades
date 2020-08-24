d150fc882ef195555b35df49a8d821ecab5e6c8f
Ruwani <ruwanide51@gmail.com>
2020-08-24 19:17:01 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -15,16 +15,13 @@ config.password = process.env.PASSWORD || configVar.password;
 require("pg").defaults.parseInt8 = true; //makes integers ints
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
-    logging: false,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
-    config,
-    { logging: false }
+    config
   );
 }
 
